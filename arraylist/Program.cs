07e3adb4a7f71f49b80cbe351618b625626ee77c
fatihtalas <112730219+fatihtalas@users.noreply.github.com>
2023-01-02 20:12:51 +0300

    Add files via upload

---
+++ arraylist/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections;
+
+namespace arraylist // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList liste=new ArrayList();
+            //liste.Add("Ayşe");
+            //liste.Add(2);
+            //liste.Add(true);
+            //liste.Add('a');
+            //Console.WriteLine(liste[1]);
+
+            //foreach (var item in liste)
+            //{
+            //   Console.WriteLine(item);
+            //}
+
+            Console.WriteLine("Add Range");
+            //string[] renkler={"kırmızı","yeşil","mavi"};
+            List<int> sayilar=new List<int>(){1,8,3,7,9,92,5};
+
+            //liste.AddRange(renkler);
+            liste.AddRange(sayilar);
+            foreach (var item in liste)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("Sort");
+            liste.Sort();
+             foreach (var item in liste)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("Binary Search");
+            Console.WriteLine(liste.BinarySearch(9));
+            Console.WriteLine("Reverse");
+            liste.Reverse();
+              foreach (var item in liste)
+            {
+                Console.WriteLine(item);
+            }
+                }
+    }
+}
---
+++ c101odev1/Program.cs
@@ -0,0 +1,138 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace c101odev1 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Soru numarasını giriniz.");
+            string soru=Console.ReadLine();
+            switch (soru)
+            {
+                case "1":
+                soru1();
+                break;
+                case "2":
+                soru2();
+                break;
+                case "3":
+                soru3();
+                break;
+                case "4":
+                soru4();
+                break;           
+            }
+        }
+        static void soru1()
+        {
+            Console.WriteLine("Soru 1:");
+            Console.WriteLine("Pozitif bir sayı giriniz.");
+            int sayi=0;
+            try
+            {
+            sayi=Convert.ToInt32(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Geçersiz veri.");
+                soru1();
+            }
+            int[] dizi=new int[sayi];
+            for(int i=0;i<dizi.Length;i++)
+            {
+                try
+            {
+                Console.WriteLine("{0}. sayıyı giriniz.",i+1);
+            dizi[i]=Convert.ToInt32(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Geçersiz veri tekrar giriniz.");
+                i--;
+            }
+            }
+            for(int i=0;i<dizi.Length;i++)
+            {
+                if(dizi[i]%2==0)
+                Console.WriteLine(dizi[i]);
+            }
+        }
+        static void soru2()
+        {
+            Console.WriteLine("Soru 2:");
+            int sayi1=0,sayi2=0;
+            try
+            {
+                Console.WriteLine("1. Sayıyı giriniz.");
+            sayi1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("2. Sayıyı giriniz.");
+            sayi2=Convert.ToInt32(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Geçersiz veri.");
+                soru2();
+            }
+
+            int[] dizi=new int[sayi1];
+            for(int i=0;i<dizi.Length;i++)
+            {
+                try
+            {
+                Console.WriteLine("Dizinin {0}. sayısını giriniz.",i+1);
+            dizi[i]=Convert.ToInt32(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Geçersiz veri tekrar giriniz.");
+                i--;
+            }
+            }
+            for(int i=0;i<dizi.Length;i++)
+            {
+                if(dizi[i]%sayi2==0)
+                Console.WriteLine(dizi[i]);
+            }
+
+        }
+        static void soru3()
+        {
+            Console.WriteLine("Soru 3:");
+            Console.WriteLine("Pozitif bir sayı giriniz.");
+            int sayi=0;
+            try
+            {
+            sayi=Convert.ToInt32(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Geçersiz veri.");
+                soru3();
+            }
+            string[] dizi=new string[sayi];
+            for(int i=0;i<dizi.Length;i++)
+            {
+             
+                Console.WriteLine("{0}. kelimeyi giriniz.",i+1);
+                dizi[i]=Console.ReadLine();
+            
+            }
+            for(int i=dizi.Length-1;i>=0;i--)
+            {                
+                Console.WriteLine(dizi[i]);
+            }
+        }
+        static void soru4()
+        {
+            Console.WriteLine("Soru 4:");
+            Console.WriteLine("Bir cümle yazınız.");
+            string cumle=Console.ReadLine();
+            string[] dizi=cumle.Split(" ");
+            Console.WriteLine("Bu cümle {0} kelimeden oluşur.",dizi.Length);
+            string yenicumle=String.Join("",dizi);
+            Console.WriteLine("Bu cümle {0} harften oluşur.",yenicumle.Length);
+        }
+    }
+}
