0720f95b6ac482a127ea0903d4f9ae8aaf714905
fatihtalas <112730219+fatihtalas@users.noreply.github.com>
2023-01-02 20:18:03 +0300

    Add files via upload

---
+++ donguler_1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace donguler_1 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Console.WriteLine("Lutfen bir sayı giriniz.");
+            int sayac=int.Parse(Console.ReadLine());
+            for (int i = 0; i <= sayac; i++)
+            {
+                if(i%2==1)
+                Console.WriteLine(i);
+            }
+            int tektoplam=0, cifttoplam=0;
+            for (int i = 0; i <= 1000; i++)
+            {
+                if(i%2==1)   
+                tektoplam=tektoplam+i;
+                else
+                cifttoplam=cifttoplam+i;             
+            }
+
+            Console.WriteLine("Tek Toplam:"+tektoplam.ToString());
+            Console.WriteLine("Çift Toplam:"+cifttoplam.ToString()); */
+            for (int i = 0; i < 10; i++)
+            {
+                if(i==4)
+                break;
+                Console.WriteLine(i);
+            }
+             for (int i = 0; i < 10; i++)
+            {
+                if(i==4)
+                continue;
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ donguler_while_foreach/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace donguler_while_foreach // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Lütfen Bir Sayı Giriniz:");
+            // int sayi=int.Parse(Console.ReadLine());
+            // int sayac=1,toplam=0;
+            // while(sayac<=sayi)
+            // {
+            //     toplam+=sayac;
+            //     sayac++;
+            // }
+            // Console.WriteLine(toplam/sayi);
+
+            // char character='a';
+            // while(character<'z')
+            // {
+            //     Console.WriteLine(character);
+            //     character++;
+            // }
+
+            Console.WriteLine("*****Foreach*****");
+            string[] arabalar={"BMW","Ford","Toyota","Nissan"};
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+        }
+    }
+}
