d5edb404db7f62470fe20daa0e22afd708d45386
fatihtalas <112730219+fatihtalas@users.noreply.github.com>
2023-01-02 20:30:38 +0300

    Add files via upload

---
+++ metotlar/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace metotlar // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=2,b=3;
+            Console.WriteLine(a+b);
+
+            int sonuc=Topla(a,b);
+            Console.WriteLine(sonuc);
+            Metotlar ornek=new Metotlar();
+            ornek.ekranayaz(sonuc.ToString());
+
+            int sonuc2=ornek.arttirvetopla(ref a,ref b);
+            ornek.ekranayaz(sonuc2.ToString());
+            ornek.ekranayaz(Convert.ToString(a+b));
+        }
+        static int Topla(int deger1,int deger2)
+        {
+            return (deger1+deger2);
+        }
+        
+    }
+    class Metotlar
+    {
+        public void ekranayaz(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+        public int arttirvetopla(ref int deger1,ref int deger2)
+        {
+            deger1+=1;
+            deger2+=1;
+            return deger1+deger2;
+        }
+    }
+}
---
+++ overloading/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace overloading // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sayi="999";
+            bool sonuc=int.TryParse(sayi,out int outsayi);
+            if(sonuc)
+            {
+                Console.WriteLine("Başarılı");
+                Console.WriteLine(outsayi);
+            }
+            else
+            Console.WriteLine("Başarısız");
+            Metotlar instance=new Metotlar();
+            instance.Topla(4,5,out int toplamsonuc);
+            Console.WriteLine(toplamsonuc);
+
+            int ifade=999;
+            instance.ekranayazdir(ifade.ToString());
+            instance.ekranayazdir(ifade);
+            instance.ekranayazdir("Fatih","Talas");
+
+        }
+    }
+    class Metotlar
+    {
+        public void Topla(int a,int b,out int toplam)
+        {
+            toplam=a+b;
+        }
+
+        public void ekranayazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+        public void ekranayazdir(int veri)
+        {
+            Console.WriteLine(veri);
+        }
+         public void ekranayazdir(string veri1,string veri2)
+        {
+            Console.WriteLine(veri1+veri2);
+        }
+    }
+}
